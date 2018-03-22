d3951e132abab4815dd175db6ef316b9c94a2662
Kieran <kieran.a.gibb@gmail.com>
2018-03-22 18:41:55 +0000

    filter out duplicate recipients

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const flumeView = require('flumeview-reduce')
 const pull = require('pull-stream')
 const get = require('lodash/get')
 
-const FLUME_VIEW_VERSION = 1.0
+const FLUME_VIEW_VERSION = 1.3
 const FLUME_VIEW_NAME = "invites"
 const MSG_TYPE = "invite"
 
@@ -14,7 +14,6 @@ module.exports = {
     stream: 'source'
   },
   init: function (ssbServer, config) {
-    console.log("*** Loaded ***")
     const view = ssbServer._flumeUse(
       FLUME_VIEW_NAME,
       flumeView(FLUME_VIEW_VERSION, reduce, map, null, {})
@@ -29,13 +28,13 @@ module.exports = {
 
 function reduce (accumulator, item) {
   var accumulator = accumulator || {}
-  const { author, root, recipient } = item
-  if (root) {
-    var invites = get(accumulator, [root], new Set())
-    invites.add(recipient)
+  const { root, recipient } = item
+  if (root && recipient) {
+    var invites = get(accumulator, [root], new Array())
+    invite = invites.filter(invite => invite === recipient).length > 0
+    if (!invite) invites.push(recipient)
     accumulator[root] = invites
   }
-  console.log(accumulator)
   return accumulator
 }
 
@@ -43,7 +42,6 @@ function map (msg) {
   const { author, content } = msg.value
   if (get(content, 'type') !== MSG_TYPE) return null
   return {
-    author: author,
     root: content.root,
     recipient: content.recipient.link
   }
